2fdd488f63b2963f42afc762a0a0fd0bcb7eba80
Vijaymd0007 <vijaymd0007@gmail.com>
2023-06-09 11:44:46 +0530

    initial coomit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import Navbar from "./components/Navbar.js"
+import Service from "./routes/Service";
+import Home from "./routes/Home";
+import Contact from "./routes/Contact";
+import About from "./routes/About";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    return(
+        <div className = "App">
+            
+                <Routes>
+                    <Route path="/" element={ <Home/>}/>
+                    <Route path="/about" element={ <About/>}/>
+                    <Route path="/service" element={ <Service/>}/>
+                    <Route path="/contact" element={ <Contact/>}/>
+                </Routes>
+            
+                
+               
+               
+           
+        </div>
+    )
 }
-
-export default App;
